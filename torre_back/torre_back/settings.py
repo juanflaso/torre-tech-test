bea9bac5d86bbb8b8f7d4876b723f930b84c4c2d
Juan Francisco Laso <juanflaso1@hotmail.com>
2021-04-16 23:04:05 -0500

    Functional feed endpoint

--- torre_back/torre_back/settings.py
+++ torre_back/torre_back/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'feed',
+
+    'rest_framework',
+    'requests',
 ]
 
 MIDDLEWARE = [
